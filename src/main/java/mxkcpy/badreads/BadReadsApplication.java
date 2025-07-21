3a2842c406e174382a091e94cb38ac50523892cc
mxkcpy <mxkcpy@localhost.localdomain>
2025-07-21 16:56:33 +0200

    initial commit

---
+++ src/main/java/mxkcpy/badreads/BadReadsApplication.java
@@ -0,0 +1,13 @@
+package mxkcpy.badreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadReadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadReadsApplication.class, args);
+    }
+
+}

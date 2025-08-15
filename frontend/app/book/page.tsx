9f144eb38c95fd21bf3a2d225f7aa78f6f81479d
mxkcpy <mxkcpy@localhost.localdomain>
2025-08-15 14:51:27 +0200

    minor fix for github actions

--- frontend/app/book/page.tsx
+++ frontend/app/book/page.tsx
@@ -16,7 +16,7 @@ export default function BookDetailsPage() {
 useEffect(() => {
   if (!bookId) return;
 
-  fetch(`http://localhost:8080/book?id=${bookId}`)
+  fetch(`http://192.168.100.203:8080/book?id=${bookId}`)
     .then(async (res) => {
       if (!res.ok) {
         const errData = await res.json();

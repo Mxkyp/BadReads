b7c98b7decbacf888342699f0902980d64e76496
mxkcpy <mpawlos20@gmail.com>
2025-09-21 15:47:21 +0200

    apart from images cards showup in a ok way

--- server/new_frontend_router/app/discover/discoverFetcher.tsx
+++ server/new_frontend_router/app/discover/discoverFetcher.tsx
@@ -24,7 +24,7 @@ function Fetch() {
   return (
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {data.map((book) => (
-            <article key={book.id} className="flex max-w-xl flex-col items-start justify-between">
+            <article key={book.id} className="flex mx-5 max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
                 <a
                   href={book.metadata.subtitle}

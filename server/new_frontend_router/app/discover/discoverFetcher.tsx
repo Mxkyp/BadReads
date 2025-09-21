1396a40911ef451aa2748525048346a287ae3dc3
mxkcpy <mpawlos20@gmail.com>
2025-09-21 16:22:33 +0200

    added nice ui-animations

--- server/new_frontend_router/app/discover/discoverFetcher.tsx
+++ server/new_frontend_router/app/discover/discoverFetcher.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { motion } from "framer-motion";
 
 const queryClient = new QueryClient()
 
@@ -24,6 +25,13 @@ function Fetch() {
   return (
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {data.map((book) => (
+
+            <motion.div
+              key={book.id}
+              whileHover={{ scale: 1.05 }}
+              className="cursor-pointer"
+              onClick={() => navigate(`/book?id=${book.id}`)}
+            >
             <article key={book.id} className="flex mx-5 max-w-xl flex-col p-8 items-start justify-between bg-white shadow rounded-2xl overflow-hidden">
               <img
                 src={book.metadata.thumbnailURL}
@@ -49,6 +57,7 @@ function Fetch() {
                 <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{book.metadata.description}</p>
               </div>
             </article>
+            </motion.div>
           ))}
         </div>
   )

dd2b945e7b49c1e7c23b114b081d5360e25a5a76
mxkcpy <mpawlos20@gmail.com>
2025-09-21 15:07:26 +0200

    fetching books works, now have to specify the layout

--- server/new_frontend_router/app/discover/discoverFetcher.tsx
+++ server/new_frontend_router/app/discover/discoverFetcher.tsx
@@ -22,8 +22,40 @@ function Fetch() {
   if (error) return 'An error has occurred: ' + error.message
 
   return (
-    <div>
-      <h1>{data[0].id}</h1>
-    </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {data.map((book) => (
+            <article key={book.id} className="flex max-w-xl flex-col items-start justify-between">
+              <div className="flex items-center gap-x-4 text-xs">
+                <a
+                  href={book.metadata.subtitle}
+                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                >
+                  {book.metadata.genres[0]}
+                </a>
+              </div>
+              <div className="group relative grow">
+                <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                  <a href={book.metadata.subtitle}>
+                    <span className="absolute inset-0" />
+                    {book.metadata.title}
+                  </a>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{book.metadata.description}</p>
+              </div>
+              <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                <img alt="" src={book.metadata.thumbnailURL} className="size-10 rounded-full bg-gray-50" />
+                <div className="text-sm/6">
+                  <p className="font-semibold text-gray-900">
+                    <a href={book.metadata.subtitle}>
+                      <span className="absolute inset-0" />
+                      {book.metadata.subtitle}
+                    </a>
+                  </p>
+                  <p className="text-gray-600">{book.metadata.subtitle}</p>
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
   )
 }

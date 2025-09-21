c105d521244e7001ca90189903b3e86ba121ad9a
mxkcpy <mpawlos20@gmail.com>
2025-09-21 15:55:31 +0200

    looks ok-ish

--- server/new_frontend_router/app/discover/discoverFetcher.tsx
+++ server/new_frontend_router/app/discover/discoverFetcher.tsx
@@ -25,6 +25,11 @@ function Fetch() {
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {data.map((book) => (
             <article key={book.id} className="flex mx-5 max-w-xl flex-col items-start justify-between">
+              <img
+                src={book.metadata.thumbnailURL}
+                alt={book.metadata.title}
+                className="w-auto h-100 object-cover rounded-xl mb-2"
+              />
               <div className="flex items-center gap-x-4 text-xs">
                 <a
                   href={book.metadata.subtitle}
@@ -43,7 +48,6 @@ function Fetch() {
                 <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{book.metadata.description}</p>
               </div>
               <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-                <img alt="" src={book.metadata.thumbnailURL} className="size-10 rounded-full bg-gray-50" />
                 <div className="text-sm/6">
                   <p className="font-semibold text-gray-900">
                     <a href={book.metadata.subtitle}>

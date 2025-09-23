d46a4aae872aaaa61eec91c416c53e58d8f5b09a
mxkcpy <mpawlos20@gmail.com>
2025-09-24 00:40:54 +0200

    deleted the old frontend

--- server/frontend/src/components/ui/input.tsx
+++
@@ -1,8 +0,0 @@
-export function Input({ ...props }) {
-  return (
-    <input
-      className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring focus:border-blue-300"
-      {...props}
-    />
-  );
-}

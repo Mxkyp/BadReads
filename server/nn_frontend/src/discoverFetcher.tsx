16706edb176cea49177aed4e38c6ad9e8ed28cc8
mxkcpy <mpawlos20@gmail.com>
2025-09-23 11:33:50 +0200

    made the discovery view more presentable

--- server/nn_frontend/src/discoverFetcher.tsx
+++ server/nn_frontend/src/discoverFetcher.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
-import { motion } from "framer-motion";
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 const queryClient = new QueryClient()
 
@@ -37,44 +39,25 @@ function Fetch() {
   }
 
   let bookData: Book[] = data;
-
+  //TODO: Add variable number of columns based on screen size
   return (
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+    <div className="mx-auto mt-10 flex max-w-2xl justify-items-center border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none ">
+      <Box sx={{ overflowY: 'scroll' }}>
+        <ImageList variant="masonry" cols={8} gap={8}>
           {bookData.map((book: Book) => (
-
-            <motion.div
-              key={book.id}
-              whileHover={{ scale: 1.05 }}
-              className="cursor-pointer"
-            >
-            <article key={book.id} className="flex mx-5 max-w-xl flex-col p-8 items-start justify-between bg-white shadow rounded-2xl overflow-hidden">
+            <ImageListItem key={book.id}>
               <img
-                src={book.metadata.thumbnailURL}
+                className="hover:scale-105 transition duration-400"
+                srcSet={`${book.metadata.thumbnailURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                src={`${book.metadata.thumbnailURL}?w=248&fit=crop&auto=format`}
                 alt={book.metadata.title}
-                className="w-auto h-85 object-cover rounded-xl mb-2 place-self-center"
+                loading="lazy"
               />
-              <div className="flex items-center gap-x-4 text-xs">
-                <a
-                  href={book.metadata.subtitle}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {book.metadata.genres[0]}
-                </a>
-              </div>
-              <div className="group relative grow">
-                <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                  <a href={book.metadata.title}>
-                    <span className="absolute inset-0" />
-                    {book.metadata.title}
-                  </a>
-                </h3>
-                {book.metadata.subtitle}
-                <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{book.metadata.description}</p>
-              </div>
-            </article>
-            </motion.div>
+            </ImageListItem>
           ))}
-        </div>
+        </ImageList>
+      </Box>
+    </div>
   )
 }
 

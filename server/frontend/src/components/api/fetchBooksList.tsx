20925ef6ad9c75e5056cb0a81d863a1cd6ab6052
mxkcpy <mpawlos20@gmail.com>
2025-09-26 14:24:01 +0200

    added spinner, and dummy error messages if loading goes wrong

--- server/frontend/src/components/api/fetchBooksList.tsx
+++ server/frontend/src/components/api/fetchBooksList.tsx
@@ -2,6 +2,9 @@ import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-quer
 import Box from '@mui/material/Box';
 import { type Book } from '../types';
 import BookList  from '../BookList.tsx';
+import { RingLoader } from "react-spinners";
+import "../css/index.css"
+import ErrorPage from "../pages/error.tsx"
 
 const queryClient = new QueryClient();
 
@@ -22,13 +25,18 @@ function Fetch() {
     staleTime: Infinity
   })
 
-  if (isLoading) return 'Loading...'
+  if (isLoading) return (
+    <div className="mx-auto mt-10 flex items-center justify-center flex-row flex-column max-w-2xl border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none ">
+      <RingLoader className="mt-30 self-center" color="var(--color-dark)" loading={isLoading} size={300} />
+    </div>
+  );
+  
+  //TODO: make this a redirect to a err page
+  
+  if (error) return <ErrorPage code="500" title="whoops!" description="It appears my there is a problem with the server. I am on it! ~ Mikolaj"/>;
 
-  if (error) return 'An error has occurred: ' + error.message
-
-  //TODO: proper err handling
   if (data === undefined) {
-    return "error reading data"
+     return <ErrorPage code="500" title="whoops!" description="It appears my there is a problem with the server. I am on it! ~ Mikolaj"/>
   }
 
   let bookData: Book[] = data;
